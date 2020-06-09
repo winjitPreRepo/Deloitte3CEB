02e4dd1e7d33b75c268d61f59238fa7591a28185
Rohit Khairnar <44599527+rohitkhairnarrsk2@users.noreply.github.com>
2020-06-09 14:27:38 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+/*!
+ *
+ * Angle - Bootstrap Admin Template
+ *
+ * Version: 4.6
+ * Author: @themicon_co
+ * Website: http://themicon.co
+ * License: https://wrapbootstrap.com/help/licenses
+ *
+ */
+
+import './vendor.ts';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+    enableProdMode();
+}
+
+export function getBaseUrl() {
+    return document.getElementsByTagName('base')[0].href;
+}
+
+const providers = [{ provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }];
+
+let p = platformBrowserDynamic(providers).bootstrapModule(AppModule);
+p.then(() => { (<any>window).appBootstrap && (<any>window).appBootstrap(); })
+// .catch(err => console.error(err));
